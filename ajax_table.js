8afeaa584429d02e01e0e8fa8a0990af557ab9d5
player259 <player259@mail.ru>
2013-08-31 12:05:12 +1100

    0.9

---
+++ ajax_table.js
@@ -0,0 +1,21 @@
+(function ($) {
+  // If there is AJAX on the page
+  if (Drupal.ajax != undefined) {
+    Drupal.ajax.prototype.commands.proper_replace = function (ajax, response, status) {
+      $(response.selector).replaceWith(response.data);
+      Drupal.attachBehaviors(response.selector);
+      //Drupal.attachBehaviors();      
+    }
+  }
+  // Graceful degradation
+  Drupal.behaviors.ajax_table = {
+    attach: function (context, settings) {
+      $('.ajax-table-table-wrapper a.ajax-table-link')
+        .add('.ajax-table-pager-wrapper a.ajax-table-link')
+        .each(function() {
+          if ($(this).hasClass('ajax-table-ajax-link')) $(this).show();
+          if ($(this).hasClass('ajax-table-non-js-link')) $(this).hide();
+        });
+    }
+  };
+})(jQuery);
